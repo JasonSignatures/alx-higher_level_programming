4e0ce16b917cd00445c690566ffeb0da8a6a84e4
Enobong Jackson <106785757+JasonSignatures@users.noreply.github.com>
2022-10-31 20:12:34 +0100

    made files executable

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,45 +1,73 @@
 #!/usr/bin/python3
-"""
-module
+""" Module that contains class Square,
+inheritance of class Rectangle
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square"""
+    """ Class Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ check inputs """
+        """ Initializes instances """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
-        """ check inputs """
-        return super().width
+        """ Getter size """
+        return self.width
 
     @size.setter
     def size(self, value):
-        """ check inputs """
-        Rectangle.width.fset(self, value)
-        Rectangle.height.fset(self, value)
+        """ Setter size """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
 
     def update(self, *args, **kwargs):
-        """ check inputs """
-        if kwargs and (args is None or len(args) == 0):
-            if 'size' in kwargs.keys():
-                aux = {'width': kwargs['size'], 'height': kwargs['size']}
-                kwargs.update(aux)
-            super().update(*args, **kwargs)
+        """ update method """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
         else:
-            args_list = list(args)
-            if len(args) > 1:
-                args_list.insert(1, args[1])
-            super().update(*args_list, **kwargs)
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """ check inputs """
-        new_dict = {'id': self.id,
-                    'size': self.height,
-                    'x': self.x,
-                    'y': self.y}
-        return new_dict
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
